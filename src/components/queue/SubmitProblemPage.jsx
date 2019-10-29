19b4b0644f5eab57a03b5344e5319d186ac1b4c9
yandrey <andrej.rostek@gmail.com>
2019-10-29 22:37:02 +0100

    linky po submitnuti problemu

--- src/components/queue/SubmitProblemPage.jsx
+++ src/components/queue/SubmitProblemPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from "react-router-dom"
 
 import SubmitProblemForm from './SubmitProblemPage/SubmitProblemForm'
 
@@ -9,23 +10,32 @@ const mapStateToProps = (state, ownProps) => ({
 	queue: ownProps.queue,
 	token: ownProps.token,
 	stage: state.content.submitProblem.page.stage,
-	message: state.content.submitProblem.page.message
+	message: state.content.submitProblem.page.message,
+	problemId: state.content.submitProblem.page.problemId
 })
-
+//mozno to tu bude chciet reset, po submitnuti problemu totiz tato page zostane na COMPLETED case a nezmeni sa dokym nereloadnem
 class SubmitProblemPage extends React.Component {
 	render() {
+		const queue = this.props.queue
+		const message = this.props.message
+		const problemId = this.props.problemId
+		const token = this.props.token
 		switch(this.props.stage) {
 			case submitProblemStages.COMPLETED:
 				return(
-					<div>{this.props.message}</div>
+					<div>
+						{message}
+						<p>Would you like to check out <Link to={'/problem/' + problemId}>your submitted problem</Link> or <Link to={'/q/' + queue}>the {queue} queue</Link>?</p>
+					</div>
+
 				)
 			case submitProblemStages.SUBMITTING_FORM:
 			default:
 				return(
 					<div>
-						<p>Submitting new problem to queue {this.props.queue}</p>
-						<SubmitProblemForm queue={this.props.queue} token={this.props.token}/>
-						<div>{this.props.message}</div>
+						<p>Submitting new problem to queue {queue}</p>
+						<SubmitProblemForm queue={queue} token={token}/>
+						<div>{message}</div>
 					</div>
 				)
 		}
