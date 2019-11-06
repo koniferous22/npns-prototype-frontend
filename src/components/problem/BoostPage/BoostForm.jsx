38a005fc65989c2fc9a711efc0553cf9a61f6a3f
yandrey <andrej.rostek@gmail.com>
2019-11-06 11:10:25 +0100

    Boost validacia, linky na problem page

--- src/components/problem/BoostPage/BoostForm.jsx
+++ src/components/problem/BoostPage/BoostForm.jsx
@@ -9,13 +9,34 @@ let boost = (values, dispatch, props) => {
 	)
 }
 
+const number = value =>
+	isNaN(Number(value)) ? 'Must be a number' : undefined
+const positive = value =>
+	value > 0 ? undefined : 'Must be a positive number'
+
+const renderField = ({
+	input,
+	label,
+	type,
+	meta: { touched, error, warning }
+}) => (
+	<div>
+		<label>{label}</label>
+		<div>
+			<input {...input} placeholder={label} type={type} />
+			{touched &&
+				((error && <span>{error}</span>) ||
+				(warning && <span>{warning}</span>))}
+		</div>
+	</div>
+)
+
 let BoostForm = props => {
 	const { handleSubmit } = props;
 	return (<form onSubmit={handleSubmit}>
 		<div>
 			<label htmlFor="boost">Boost value</label>
-			<Field name="boost" component="input" type="text" />
-			{/*ide tu zmenit type?*/}
+			<Field name="boost" component={renderField} validate={[number, positive]} type="number" step="any"/>
 		</div>
 		<button type="submit">Boost!</button>
 	</form>)
