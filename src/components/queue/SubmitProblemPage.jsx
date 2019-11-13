743f7cf0690ce73a966328d15ebeceff2848ae68
yandrey <andrej.rostek@gmail.com>
2019-11-13 17:00:30 +0100

    added preview when submitting problem

--- src/components/queue/SubmitProblemPage.jsx
+++ src/components/queue/SubmitProblemPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import { connect } from 'react-redux';
 import { Link } from "react-router-dom"
 
@@ -6,7 +7,7 @@ import SubmitProblemForm from './SubmitProblemPage/SubmitProblemForm'
 import { submitProblemActions } from '../../actions/content/submitProblem'
 import { submitProblemStages } from '../../constants/content/submitProblemPage' 
 
-const mapStateToProps = state => state.content.submitProblem.page
+const mapStateToProps = state => state.content.submitProblem
 
 const mapDispatchToProps = dispatch => ({
 	reset: () => dispatch(submitProblemActions.reset()),
@@ -21,24 +22,31 @@ class SubmitProblemPage extends React.Component {
 	componentWillUnmount() {
 		this.props.reset()
 	}
-
 	render() {
-		switch(this.props.stage) {
+		const page = this.props.page
+		const form = this.props.form.form
+		switch(page.stage) {
 			case submitProblemStages.COMPLETED:
 				return(
 					<div>
-						{this.props.message}
-						<p>Would you like to check out <Link to={'/problem/' + this.props.problemId}>your submitted problem</Link> or the <Link to={'/q/' + this.props.queue}>{this.props.queue} queue</Link>?</p>
+						{page.message}
+						<p>Would you like to check out <Link to={'/problem/' + page.problemId}>your submitted problem</Link> or the <Link to={'/q/' + page.queue}>{page.queue} queue</Link>?</p>
 					</div>
 
 				)
 			case submitProblemStages.SUBMITTING_FORM:
 			default:
+				console.log('lulik')
 				return(
 					<div>
 						<p>Submitting new problem</p>
-						<SubmitProblemForm defaultQueue={this.props.urlQueue || 'Index'} token={this.props.token} queueOptions={this.props.queueOptions}/>
-						<div>{this.props.message}</div>
+						<SubmitProblemForm defaultQueue={this.props.urlQueue || 'Index'} token={this.props.token} queueOptions={page.queueOptions}/>
+						<div>{page.message}</div>
+						<div>
+							<p>Preview</p>
+							{form && form.values && <h3>{form.values.title}</h3>}
+							<p>{form && form.values && <ReactMarkdown source={form.values.description} />}</p>
+						</div>
 					</div>
 				)
 		}
