1e13f83a79f1ce651527a15a80294ec1455acb22
koniferous22 <dekkel22@gmail.com>
2019-11-26 21:17:46 +0100

    Lol heroku presentation deploy

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,3 +1,1 @@
-export const appConfig = { backendUrl : 'http://localhost:3000'};
-//export const appConfig = { backendUrl : "http://192.168.0.100:3000"};
-//export const appConfig = { backendUrl : "http://192.168.93.199:3000"};
+export const appConfig = { backendUrl : 'https://npns-prototype-api.herokuapp.com/'};
