a430ca5e9feb7164b8fb4dfe300db5da20581234
koniferous22 <dekkel22@gmail.com>
2019-12-07 19:54:26 +0100

    Hotfix problem box overflowing title

--- src/components/problem/ProblemBox.jsx
+++ src/components/problem/ProblemBox.jsx
@@ -5,22 +5,25 @@ import StyledProblemBox from '../../styled-components/problem-related/ProblemBox
 import ProblemBoxMeta from '../../styled-components/problem-related/ProblemBoxMeta'
 import SolvedLabel from '../../styled-components/problem-related/SolvedLabel'
 
-export const ProblemBox = (props) => (
-	<StyledProblemBox>
-		<ProblemBoxMeta>
-			<li>{new Date(props.created).toLocaleDateString()}</li>
-			<li>{'Bounty: ' + props.bounty + '$'}</li>
-			<li>
-				{!props.active ? <SolvedLabel>SOLVED</SolvedLabel> : (
-					props.loggedIn && <Link to={'/problem/' + props.id + '/boost'}>Boost</Link>
-				)}
-			</li>
-		</ProblemBoxMeta>
-		<h4><Link to={{pathname: "/problem/" + props.id, id: props.id}}>{props.title}</Link></h4>
-		<ProblemBoxMeta>
-			<li>{props.viewCount + ' view' + ((props.viewCount !== 1) ? 's' : '')}</li>
-			<li>{'Replies: ' + props.submissionCount}</li>
-			<li><Link to={'/u/' + props.username}>{props.username}</Link></li>
-		</ProblemBoxMeta>
-	</StyledProblemBox>
-)
+export const ProblemBox = (props) => {
+	const displayedTitle = props.title.length > 50 ? props.title.substring(0, 50) + '...' : props.title
+	return (
+		<StyledProblemBox>
+			<ProblemBoxMeta>
+				<li>{new Date(props.created).toLocaleDateString()}</li>
+				<li>{'Bounty: ' + props.bounty + '$'}</li>
+				<li>
+					{!props.active ? <SolvedLabel>SOLVED</SolvedLabel> : (
+						props.loggedIn && <Link to={'/problem/' + props.id + '/boost'}>Boost</Link>
+					)}
+				</li>
+			</ProblemBoxMeta>
+			<h4><Link to={{pathname: "/problem/" + props.id, id: props.id}}>{displayedTitle}</Link></h4>
+			<ProblemBoxMeta>
+				<li>{props.viewCount + ' view' + ((props.viewCount !== 1) ? 's' : '')}</li>
+				<li>{'Replies: ' + props.submissionCount}</li>
+				<li><Link to={'/u/' + props.username}>{props.username}</Link></li>
+			</ProblemBoxMeta>
+		</StyledProblemBox>
+	)
+}
