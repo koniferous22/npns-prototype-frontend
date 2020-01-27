8946e203c46aded865b0f2a011c17050d9825abb
yandrey <andrej.rostek@gmail.com>
2020-01-27 16:11:21 +0100

    image preview, orig image sa zobrazi az po kliknuti

--- src/components/upload/ShowAttachment.jsx
+++ src/components/upload/ShowAttachment.jsx
@@ -6,14 +6,19 @@ class ShowAttachment extends React.Component {
 		var attachmentName = ''
 		if (attachmentUrl) {
 			const urlArray = attachmentUrl.split('/')
-			attachmentName = urlArray[urlArray.length -1]
+			attachmentName = urlArray[urlArray.length - 1]
+			var isImage = Boolean(attachmentUrl.match(/.(jpg|jpeg|png|gif|bmp|ico|svg)$/i))
+			if (isImage) {
+				urlArray[urlArray.length - 2] = 'w_250,c_lfill'
+				var previewUrl = urlArray.join('/')
+			}
 		}
 		return (
 			<div>
 				{
 					(attachmentUrl) ?
-						((attachmentUrl.match(/.(jpg|jpeg|png|gif|bmp|ico|svg)$/i)) ?
-							(<img src={attachmentUrl} alt="attachment" />) 
+						(isImage ?
+							(<a href={attachmentUrl}><img src={previewUrl} alt="attachment" /></a>) 
 							: 
 							(<a href={attachmentUrl}>{attachmentName}</a>)) 
 						:
