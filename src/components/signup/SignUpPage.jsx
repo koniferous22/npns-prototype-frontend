9e90ca758403af6abe9dc93420ad07098fb03037
yandrey <andrej.rostek@gmail.com>
2020-03-22 14:43:17 +0100

    som nepridal lol

--- src/components/signup/SignUpPage.jsx
+++ src/components/signup/SignUpPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { withRouter } from 'react-router'
 import queryString from 'query-string'
 
@@ -10,45 +10,43 @@ import { signupActions } from '../../actions/content/signup'
 import ContentDiv from '../../styled-components/defaults/ContentDiv'
 import BackendMessage from '../../styled-components/defaults/BackendMessage'
 
-const mapStateToProps = state => state.content.signup.page
+const SignUpPage = ({ location }) => {
+	const { stage, message, messageType } = useSelector(state => state.content.signup.page)
+	const dispatch = useDispatch()
 
-const mapDispatchToProps = dispatch => ({
-	reset: () => dispatch(signupActions.reset())
-})
+	useEffect(() => {
+		return () => {
+			dispatch(signupActions.reset())
+		};
+	}, [dispatch]);
 
-class SignUpPage extends React.Component {
-	componentWillUnmount() {
-		this.props.reset()
-	}
-	render() {
-		switch(this.props.stage) {
-			case signupStages.COMPLETED:
-				return (
-					<ContentDiv>
-						<BackendMessage messageType={this.props.messageType}>
-							{this.props.message.message}
-						</BackendMessage>
-						<ol>
-							{this.props.message.steps.map((step, i) => (
-								<li key={i}>{step}</li>
-							))}
-						</ol>
-					</ContentDiv>
-				)
-			case signupStages.SUBMITTING_FORM:
-			default:
-				const referred_by = queryString.parse(this.props.location.search).referred_by
-				return (
-					<ContentDiv>
-						<BackendMessage messageType={this.props.messageType}>
-							{this.props.message}
-						</BackendMessage>
-						<h1>REGISTRUJ SA PRIATELU</h1>
-						<SignUpForm initialValues={{referred_by: referred_by}} />
-					</ContentDiv>
-				)
-		}
+	switch(stage) {
+		case signupStages.COMPLETED:
+			return (
+				<ContentDiv>
+					<BackendMessage messageType={messageType}>
+						{message.message}
+					</BackendMessage>
+					<ol>
+						{message.steps.map((step, i) => (
+							<li key={i}>{step}</li>
+						))}
+					</ol>
+				</ContentDiv>
+			)
+		case signupStages.SUBMITTING_FORM:
+		default:
+			const referred_by = queryString.parse(location.search).referred_by
+			return (
+				<ContentDiv>
+					<BackendMessage messageType={messageType}>
+						{message}
+					</BackendMessage>
+					<h1>REGISTRUJ SA PRIATELU</h1>
+					<SignUpForm initialValues={{referred_by: referred_by}} />
+				</ContentDiv>
+			)
 	}
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SignUpPage))
+export default withRouter(SignUpPage)
