cfaff4a9c56c41472201046aba5f11439e5c9268
yandreySK <andrej.rostek@gmail.com>
2020-04-13 13:53:56 +0200

    lame submitProblem

--- src/components/queue/SubmitProblemPage/SubmitProblemForm.jsx
+++ src/components/queue/SubmitProblemPage/SubmitProblemForm.jsx
@@ -1,4 +1,62 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+
+import Input from '../../form/Input'
+import MyEditor from '../../form/MyEditor'
+import ReduxFormSelect from '../../form/ReduxFormSelect' //alebo dat toto do pice?
+import Select from 'react-select'
+
+import CenteredDiv from '../../../styled-components/defaults/CenteredDiv'
+import Button from '../../../styled-components/defaults/Button'
+
+import { submitProblem } from '../../../store/content/submitProblemPage'
+
+
+const SubmitProblemForm = ({ defaultQueue, token, queueOptions, attachmentUrls }) => {
+	const dispatch = useDispatch()
+	const { register, handleSubmit, errors } = useForm()
+
+	const transformIntoOption = q => ({value: q, label: q})
+
+	let options = queueOptions || []
+	options = options.map(transformIntoOption)
+
+	const submit = data => {
+		if (!data.queue) {
+			data.queue = transformIntoOption(defaultQueue)
+		}
+		dispatch(
+			submitProblem(
+				{
+					queue_name: data.queue.value,
+					title: data.title,
+					content: data.description,
+					attachmentUrls: attachmentUrls
+				},
+				token
+			)
+		)
+	}
+//namiesto editoru je tu docasne input a select je broken ako aj predtym
+	return (
+		<form onSubmit={handleSubmit(submit)}>
+			<CenteredDiv>
+				<Select name="queue" options={options} defaultValue={transformIntoOption(defaultQueue)} label="Select queue"/>
+				<Input name="title" type="text" label="Title" ref={register({ required: true })} errors={errors} alignLeft/>
+				<Input name="description" type="textarea" label="Description" ref={register({ required: true })} errors={errors} alignLeft/>
+			</CenteredDiv>
+			<Button type="submit">Submit</Button>
+		</form>
+	)
+}
+
+
+export default SubmitProblemForm
+
+
+/*####################################
+import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 import MyEditor from '../../form/MyEditor'
@@ -64,4 +122,4 @@ export default reduxForm({
 	onSubmit: submit,
 	validate,
 	getFormState: ({content}) => content.submitProblem.form
-})(SubmitProblemForm)
+})(SubmitProblemForm)*/
