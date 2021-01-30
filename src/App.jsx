f023a019e1fcaf7a7b319d4d76e9ecb7b3ea0ac0
andrej <andrej@localhost.localdomain>
2021-01-30 11:09:42 +0100

    first commit baby, App.js now uses hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+import React, { useEffect, useRef } from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import IEPage from './components/issues/InternetExplorer'
 
@@ -39,89 +41,80 @@ import { logout, login, verify } from './store/auth'
 
 import AppDiv from './styled-components/App'
 
+const client = new ApolloClient({
+	uri: 'http://localhost:3000/',
+	cache: new InMemoryCache()
+  });
+
 const parseUrlParam = (routeProps, paramName) => new URLSearchParams(routeProps.location.search).get(paramName)
 
-class App extends React.Component {
-	
-	componentDidMount() {
-		this.props.verify(this.props.token)
-	}
+function App() {
+	const { user, token } = useSelector(state => state.auth)
+	const dispatch = useDispatch()
 
-	componentDidUpdate(prevProps) {
-		if (prevProps.token !== this.props.token) {
-			this.props.verify(this.props.token)
+	const mounted = useRef();
+	useEffect(() => {
+		if (mounted.current) {
+			dispatch(verify(token));
+			mounted.current = true;
+		} else {
+			dispatch(verify(token));
 		}
+	}, []);
+
+	const loggedIn = !!user
+	if((navigator.userAgent.indexOf("MSIE") !== -1 ) || (!!document.documentMode === true )) { //IF IE > 10
+		return (<IEPage />)
 	}
 
-	render() {
-		
-		const loggedIn = !!this.props.user
-		if((navigator.userAgent.indexOf("MSIE") !== -1 ) || (!!document.documentMode === true )) { //IF IE > 10
-			return (<IEPage />)
-		}
-		return (
-				<Router>
-					<AppDiv>
-						<Header logout={this.props.logout} loggedIn={loggedIn} username={this.props.user ? this.props.user.username : null}/>
-						<Switch>
-							<Route exact path="/" render={() => <Homepage user={this.props.user}/>} />
-							<NonAuthRoute path="/signup" loggedIn={loggedIn} render={() => <SignUpPage/> } />
-							<NonAuthRoute path="/forgotpwd" loggedIn={loggedIn} render={(routeProps) => <ForgotPassword loggedIn={loggedIn} redirect={(routeProps.location && routeProps.location.state) ? routeProps.location.state.from : null}/>}/>
-							<Route path="/login" loggedIn={loggedIn} render={(routeProps) => <Login loggedIn={loggedIn} redirect={(routeProps.location && routeProps.location.state) ? routeProps.location.state.from : '/'}/>} />
+	return (
+			<Router>
+				<AppDiv>
+					<Header logout={dispatch(logout)} loggedIn={loggedIn} username={user ? user.username : null}/>
+					<Switch>
+						<Route exact path="/" render={() => <Homepage user={user}/>} />
+						<NonAuthRoute path="/signup" loggedIn={loggedIn} render={() => <SignUpPage/> } />
+						<NonAuthRoute path="/forgotpwd" loggedIn={loggedIn} render={(routeProps) => <ForgotPassword loggedIn={loggedIn} redirect={(routeProps.location && routeProps.location.state) ? routeProps.location.state.from : null}/>}/>
+						<Route path="/login" loggedIn={loggedIn} render={(routeProps) => <Login loggedIn={loggedIn} redirect={(routeProps.location && routeProps.location.state) ? routeProps.location.state.from : '/'}/>} />
 
-							<Route exact path="/q/:name" render={(routeProps) => <QueuePage queue={routeProps.match.params.name} loggedIn={loggedIn}/>} />
-							<PrivateRoute path="/submitProblem" render={(routeProps) => <SubmitProblemPage token={this.props.token} urlQueue={parseUrlParam(routeProps, 'q')}/>} loggedIn={loggedIn}/>
-							
-							<Route exact path="/problem/:id" render={ (routeProps) => <ProblemPage loggedIn={loggedIn} token={this.props.token} problemId={routeProps.match.params.id} user={this.props.user}/>} />
-							<Route path="/problem/:id/boost" render={(routeProps) => loggedIn && <BoostPage token={this.props.token} problemId={routeProps.match.params.id} loggedIn={loggedIn}/>} />
+						<Route exact path="/q/:name" render={(routeProps) => <QueuePage queue={routeProps.match.params.name} loggedIn={loggedIn}/>} />
+						<PrivateRoute path="/submitProblem" render={(routeProps) => <SubmitProblemPage token={token} urlQueue={parseUrlParam(routeProps, 'q')}/>} loggedIn={loggedIn}/>
+						
+						<Route exact path="/problem/:id" render={ (routeProps) => <ProblemPage loggedIn={loggedIn} token={token} problemId={routeProps.match.params.id} user={user}/>} />
+						<Route path="/problem/:id/boost" render={(routeProps) => loggedIn && <BoostPage token={token} problemId={routeProps.match.params.id} loggedIn={loggedIn}/>} />
 
-							<Route exact path="/u/:username" render={ 
-								(routeProps) => <ProfilePage 
-									viewer={this.props.user ? this.props.user.username : null}
-									user={routeProps.match.params.username}
-									loggedIn={loggedIn}
-								/> }/>
-							<Route path="/u/:username/activity" render={ 
-								(routeProps) => <ActivityPage 
-									viewer={this.props.user ? this.props.user.username : null}
-									user={routeProps.match.params.username}
-									loggedIn={loggedIn}
-								/> }/>
-							<ProfileRoute path={'/u/:username/personal'} render={(routeProps) => (<PersonalInformationPage user={routeProps.match.params.username} token={this.props.token}/>)} loggedIn={loggedIn} viewer={this.props.user ? this.props.user.username : null}/>
-							<ProfileRoute path={'/u/:username/premium'} render={(routeProps) => (<PremiumPage user={routeProps.match.params.username}/>)} loggedIn={loggedIn} viewer={this.props.user ? this.props.user.username : null}/>
-							<ProfileRoute path={'/u/:username/transactions'} render={(routeProps) => <TransactionPage user={routeProps.match.params.username} token={this.props.token}/>} loggedIn={loggedIn} viewer={this.props.user ? this.props.user.username : null}/>
-							
-							<Route path="/statistics/economy" render={() => <EconomyPage token={this.props.token}/>} loggedIn={loggedIn}/>
-							<Route path="/statistics/scoreboard/:queue" render={(routeProps) => <ScoreboardPage token={this.props.token} queue={routeProps.match.params.queue} urlPage={Number(parseUrlParam(routeProps, 'page')) || 1} />} loggedIn={loggedIn} />
+						<Route exact path="/u/:username" render={ 
+							(routeProps) => <ProfilePage 
+								viewer={user ? user.username : null}
+								user={routeProps.match.params.username}
+								loggedIn={loggedIn}
+							/> }/>
+						<Route path="/u/:username/activity" render={ 
+							(routeProps) => <ActivityPage 
+								viewer={user ? user.username : null}
+								user={routeProps.match.params.username}
+								loggedIn={loggedIn}
+							/> }/>
+						<ProfileRoute path={'/u/:username/personal'} render={(routeProps) => (<PersonalInformationPage user={routeProps.match.params.username} token={token}/>)} loggedIn={loggedIn} viewer={user ? user.username : null}/>
+						<ProfileRoute path={'/u/:username/premium'} render={(routeProps) => (<PremiumPage user={routeProps.match.params.username}/>)} loggedIn={loggedIn} viewer={user ? user.username : null}/>
+						<ProfileRoute path={'/u/:username/transactions'} render={(routeProps) => <TransactionPage user={routeProps.match.params.username} token={token}/>} loggedIn={loggedIn} viewer={user ? user.username : null}/>
+						
+						<Route path="/statistics/economy" render={() => <EconomyPage token={token}/>} loggedIn={loggedIn}/>
+						<Route path="/statistics/scoreboard/:queue" render={(routeProps) => <ScoreboardPage token={token} queue={routeProps.match.params.queue} urlPage={Number(parseUrlParam(routeProps, 'page')) || 1} />} loggedIn={loggedIn} />
 
-							
-							<Route path='/logout' render={(routeProps) => <LogoutPage loggedIn={loggedIn} redirect={(routeProps.location && routeProps.location.state) ? routeProps.location.state.from : '/login'} logout={this.props.logout}/>}/>
-							<NonAuthRoute path="/confirm/registration/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmRegistrationPage token={routeProps.match.params.token}/>} />
-							<NonAuthRoute path="/confirm/passwordChange/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmPasswordChangePage token={routeProps.match.params.token}/>} />
-							<NonAuthRoute path="/confirm/emailChange/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmEmailChangePage token={routeProps.match.params.token}/>} />
-							<NonAuthRoute path="/confirm/usernameChange/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmUsernameChangePage token={routeProps.match.params.token}/>} />
-							
-							{loggedIn && <Redirect from='/profile' to={'/u/' + this.props.user.username} />}
-							
-						</Switch>
-					</AppDiv>
-				</Router>
-		)
-	}
+						
+						<Route path='/logout' render={(routeProps) => <LogoutPage loggedIn={loggedIn} redirect={(routeProps.location && routeProps.location.state) ? routeProps.location.state.from : '/login'} logout={dispatch(logout)}/>}/>
+						<NonAuthRoute path="/confirm/registration/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmRegistrationPage token={routeProps.match.params.token}/>} />
+						<NonAuthRoute path="/confirm/passwordChange/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmPasswordChangePage token={routeProps.match.params.token}/>} />
+						<NonAuthRoute path="/confirm/emailChange/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmEmailChangePage token={routeProps.match.params.token}/>} />
+						<NonAuthRoute path="/confirm/usernameChange/:token" loggedIn={loggedIn} render={(routeProps) => <ConfirmUsernameChangePage token={routeProps.match.params.token}/>} />
+						
+						{loggedIn && <Redirect from='/profile' to={'/u/' + user.username} />}
+						
+					</Switch>
+				</AppDiv>
+			</Router>
+	)
 }
 
-
-const mapStateToProps = (state) => ({
-	// AUTH REDUCER
-	user: state.auth.user,
-	token: state.auth.token
-})
-
-const mapDispatchToProps = (dispatch) => ({
-	// AUTH
-	logout: token => dispatch(logout(token)),
-	login: (username, pwd) => dispatch(login(username, pwd)),
-	verify: token => dispatch(verify(token))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App;
